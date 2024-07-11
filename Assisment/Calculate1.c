e80e7751bf445bfca87044a00797f2068cd224b9
saloni6889 <saloniyadav6889@gmail.com>
2024-07-11 10:07:21 +0530

    assessment

--- Assisment/Calculate1.c
+++ Assisment/Calculate1.c
@@ -1,14 +1,20 @@
 //**Create menu driven calculator using function
 #include<stdio.h>
+
+
+//function for add
 add()
 {
 	int a,b;
 	printf("enter the Num A:");
-	scanf("%d",&a);
+	scanf("%d",&a);//input a
 	printf("enter the num B:");
-	scanf("%d",&b);
+	scanf("%d",&b);//input b
 	printf("\nsum=%d",a+b);
 }
+
+
+//function for substraction
 sub()
 {
 	int a,b;
@@ -18,6 +24,9 @@ sub()
 	scanf("%d",&b);
 	printf("\nsum=%d",a-b);
 }
+
+
+//function for multiplication
 mul()
 {
 	int a,b;
@@ -27,6 +36,8 @@ mul()
 	scanf("%d",&b);
 	printf("\nsum=%d",a*b);
 }
+
+//funnction for division
 div()
 {
 	int a,b;
@@ -36,9 +47,11 @@ div()
 	scanf("%d",&b);
 	printf("\nsum=%d",a/b);
 }
+
+
 main()
 {
-	int choice;
+	int choice;//data member
 	while(choice!=5)
 	{
 	printf("\n******Welcome our Calculater********:");
@@ -48,28 +61,44 @@ main()
 	   printf("\npree 4 for Division:\n");
 	   printf("\n 5 exit:\n");
 	   printf("Enter the choice :");
-	   scanf("%d",&choice);
+	   scanf("%d",&choice);//enter the choice
+	   
+	   
+	   //1-addition
     if(choice==1)
 	{
 		printf("\n******Addition\n*******");
 	add();
 		
-	}	   
+	}	
+	
+	//2-substraction   
 	else if(choice==2)
 	{
 		printf("\n*****Subtraction\n******");
 		sub();
 	}
+	
+	
+	//3-multiplication
 	else if(choice==3)
 	{
 		printf("\n******Multiplication*****\n");
 		mul();
 	}
+	
+	
+	
+	//4-division
+	
 	else if(choice==4)
 	{
 		printf("\n*******Division*******\n");
 		div();
 	}
+	
+	
+	//exit
 		else if(choice==5)
 	{
 		printf("\n*******exit*******\n");
