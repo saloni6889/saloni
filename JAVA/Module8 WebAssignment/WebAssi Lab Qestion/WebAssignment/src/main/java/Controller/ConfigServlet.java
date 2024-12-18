0f61ccfb6dd235325863e734537d0391faedd9ca
saloni6889 <saloniyadav6889@gmail.com>
2024-12-18 16:35:45 +0530

    Add files via upload

---
+++ JAVA/Module8 WebAssignment/WebAssi Lab Qestion/WebAssignment/src/main/java/Controller/ConfigServlet.java
@@ -0,0 +1,29 @@
+package Controller;
+
+import java.io.IOException;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/ConfigServlet")
+public class ConfigServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    public ConfigServlet() {
+        super();
+    }
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        String configParam = config.getServletContext().getInitParameter("contextParam");
+        System.out.println("Fetched Context Parameter: " + configParam);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.getWriter().append("ConfigServlet is working");
+    }
+}
