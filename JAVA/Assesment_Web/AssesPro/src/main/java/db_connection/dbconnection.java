6b4140f17397e4e68d25b0ff57d9009230337ade
saloni6889 <saloniyadav6889@gmail.com>
2024-12-24 16:16:39 +0530

    Add files via upload

---
+++ JAVA/Assesment_Web/AssesPro/src/main/java/db_connection/dbconnection.java
@@ -0,0 +1,17 @@
+package db_connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class dbconnection {
+	public static Connection createConnection() {
+		Connection conn = null;
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			conn =  DriverManager.getConnection("jdbc:mysql://localhost:3306/ecommerce", "root", "");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return conn;
+	}
+}
